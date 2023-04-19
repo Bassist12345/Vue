2f0e90f184bfb928fa5562ff2f1d54928b583217
Bassist12345 <129359946+Bassist12345@users.noreply.github.com>
2023-04-19 09:39:53 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img src="dmm.png" class="logo"/>
+
+    <div class="wrapper">
+      <HelloWorld msg="Hello Im Godwin!"/>
+      <HelloWorld msg="This is my Web Programing Tasks"/>
+    </div>
+  </header>
+
+  <main>
+    
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,22 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+</script>
+ 
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template> 
+    <template #heading>Activities</template>
+
+   
+    <a href="#">Table</a><br /><br />
+    <a href="#">Subscript</a><br /><br />
+    <a href="#">Facebook</a><br /><br />
+    <a href="#">Building</a><br /><br />
+    <a href="#">Javascript1</a><br /><br />
+    <a href="#">Javascript2</a>
+    
+  </WelcomeItem>
+
+</template>
